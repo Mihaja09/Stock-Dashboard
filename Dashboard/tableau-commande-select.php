4ac7159316a8b25b015084fc1d28f6b389c6c6be
Andria Mihaja <85282440+Mihaja09@users.noreply.github.com>
2022-10-13 06:28:42 +0300

    Add files via upload

---
+++ Dashboard/tableau-commande-select.php
@@ -0,0 +1,131 @@
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="entete">
+                                <h3>Sélectionner les produits</h3>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 d-flex justify-content-end">
+                            <span class="voir">
+                                <a href="#" class="text-decoration-none link-info">Voir la carte</a>
+                            </span>
+                        </div>
+                    </div>
+                    
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <ul class="nav d-flex justify-content-start">
+                                <li class="nav-item">
+                                    <input type="search" class="form-control" placeholder="Rechercher..." aria-label="Search">
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">
+                                    <i class="ikonbell2 fa-solid fa-magnifying-glass"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-6 toptab">
+                            <ul class="nav d-flex justify-content-end">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">
+                                        <i class="ikonbell2 fa-regular fa-calendar"></i>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <div class="dropdown text-end align-items-center">
+                                        <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle statut" data-bs-toggle="dropdown" aria-expanded="false">
+                                            Statut
+                                        </a>
+                                        <ul class="dropdown-menu text-small">
+                                            <li><a class="dropdown-item" href="#">Sortie: Oui</a></li>
+                                            <li><a class="dropdown-item" href="#">Sortie: Non</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card mb-0">
+                        <div class="row g-0 align-items-center">
+                            <div class="col-md-3">
+                                <img src="https://i.ibb.co/vQp0yTt/ciment-1.png" class="img-fluid rounded-start my-1" alt="Ciment">
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card-body">
+                                    <h5 class="card-title">Ciment colle blanc 25kg</h5>
+                                    <p class="card-text"><small class="text-muted">Unité de vente: Sacs</small></p>
+                                    <p class="card-text">Ciment colle 25kg blanc DRY FIX  convient pour la fixation de tout type de carreau, même les mosaïques fines, jusqu'à 70kg/m². </p>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div>
+                                    <p>Ar 225 000</p>
+                                </div>
+                                <div>
+                                    <a class="btn btn-primary" type="button" href="commande-select.php" style="font-size: 0.7rem;">Ajouter à la commande
+                                    </a>
+                                </div>   
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mb-0">
+                        <div class="row g-0 align-items-center">
+                            <div class="col-md-3">
+                                <img src="https://i.ibb.co/vQp0yTt/ciment-1.png" class="img-fluid rounded-start my-1" alt="Ciment">
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card-body">
+                                    <h5 class="card-title">Ciment colle blanc 25kg</h5>
+                                    <p class="card-text"><small class="text-muted">Unité de vente: Sacs</small></p>
+                                    <p class="card-text">Ciment colle 25kg blanc DRY FIX  convient pour la fixation de tout type de carreau, même les mosaïques fines, jusqu'à 70kg/m². </p>
+                                </div>
+                            </div>
+                            <div class="col-md-3 align-self-center">
+                                <div>
+                                    <p>Ar 225 000</p>
+                                </div>
+                                <div>
+                                    <a class="btn btn-primary" type="button" href="commande-select.php" style="font-size: 0.7rem;">Ajouter à la commande
+                                    </a>
+                                </div>   
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mb-0">
+                        <div class="row g-0 align-items-center">
+                            <div class="col-md-3">
+                                <img src="https://i.ibb.co/vQp0yTt/ciment-1.png" class="img-fluid rounded-start my-1" alt="Ciment">
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card-body">
+                                    <h5 class="card-title">Ciment colle blanc 25kg</h5>
+                                    <p class="card-text"><small class="text-muted">Unité de vente: Sacs</small></p>
+                                    <p class="card-text">Ciment colle 25kg blanc DRY FIX  convient pour la fixation de tout type de carreau, même les mosaïques fines, jusqu'à 70kg/m². </p>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div>
+                                    <p>Ar 225 000</p>
+                                </div>
+                                <div>
+                                    <a class="btn btn-primary" type="button" href="commande-select.php" style="font-size: 0.7rem;">Ajouter à la commande
+                                    </a>
+                                </div>   
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <nav>
+                            <ul class="pagination justify-content-center mt-3">
+                                <li class="page-item disabled">
+                                <a class="page-link">&laquo;</a>
+                                </li>
+                                <li class="page-item"><a class="page-link active" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item">
+                                <a class="page-link" href="#">&raquo;</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
